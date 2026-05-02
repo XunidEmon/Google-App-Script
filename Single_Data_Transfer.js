20c45c69b7747a9e601db056c44a02b551b4f162
Emon <mdxunidemon@icloud.com>
2026-05-03 04:13:13 +0600

    Add single and simple data transfer to Google Sheets API
    Implement functions to send messages and data to Google Sheets API.

---
+++ Single_Data_Transfer.js
@@ -0,0 +1,30 @@
+// Single Message Send to Google Sheets API
+```cpp
+const url = "https://script.google.com/macros/s/XXXX/exec?message=HELLO";
+
+fetch(url)
+  .then(response => response.text())
+  .then(data => {
+    console.log("Response:", data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+  ```
+
+
+// Simple Data Send to Google Sheets API
+```cpp
+let data = 123;
+
+const url = `https://script.google.com/macros/s/XXXX/exec?data=${data}`;
+
+fetch(url)
+  .then(response => response.text())
+  .then(result => {
+    console.log("Response:", result);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+
